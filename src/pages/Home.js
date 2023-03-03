bf214250faf6b3efdb22bbe48a00e6a7c4326b57
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2023-03-04 00:28:32 +0600

    real time projects added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import ProjectCard from "../components/ProjectCard";
 import ProjectForm from "../components/ProjectForm";
 import SectionTitle from "../components/SectionTitle";
 
+import { useProjectsContext } from "../hooks/useProjectsContext";
 const Home = () => {
-  const [projects, setProjects] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+  const { projects, dispatch } = useProjectsContext();
   useEffect(() => {
-    const getProjects = async () => {
-      try {
-        setLoading(true);
+    const getAllProjects = async () => {
+      const res = await fetch("http://localhost:5000/api/projects");
 
-        //data fetch
-        const res = await fetch("http://localhost:5000/api/projects/");
-        if (!res.ok) {
-          throw new Error("Something Went Wrong");
-        }
+      if (!res.ok) {
+        throw new Error("something went wrong");
+      }
 
-        const data = await res.json();
-
-        console.log(data);
-
-        setProjects(data);
-        setLoading(false);
-      } catch (err) {
-        setError(err.message);
-        setLoading(false);
+      const json = await res.json();
+      //realtime project added
+      if (res.ok) {
+        dispatch({ type: "SET_PROJECTS", payload: json });
       }
     };
-    getProjects();
-  }, []);
+    getAllProjects();
+  }, [dispatch]);
   return (
     <div className="home container mx-auto py-10 2xl:py-20 grid grid-cols-3 gap-5 2xl:gap-10">
       <div className="left col-span-2">
