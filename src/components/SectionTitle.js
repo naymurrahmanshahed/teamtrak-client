132c9db62bc769044778180b98d8b7729ec9b3cc
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2023-03-03 17:26:27 +0600

    data fetching done

---
+++ src/components/SectionTitle.js
@@ -0,0 +1,11 @@
+const SectionTitle = ({ SectionTitle }) => {
+  return (
+    <div className="Section-title">
+      <h2 className="text-4xl font-medium text-teal-400 mb-10">
+        {SectionTitle}
+      </h2>
+    </div>
+  );
+};
+
+export default SectionTitle;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react";
+import SectionTitle from "../components/SectionTitle";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    const getProjects = async () => {
+      try {
+        setLoading(true);
+
+        //data fetch
+        const res = await fetch("http://localhost:5000/api/projects/");
+        if (!res.ok) {
+          throw new Error("Something Went Wrong");
+        }
+
+        const data = await res.json();
+
+        console.log(data);
+
+        setProjects(data);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    getProjects();
+  }, []);
+  return (
+    <div className="home container mx-auto py-10 2xl:py-20 grid grid-cols-3 gap-5 2xl:gap-10">
+      <div className="left col-span-2">
+        <SectionTitle SectionTitle="All Projects" className="mb-10" />
+        <div className="project-wrapper flex gap-5 2xl:gap-10 flex-wrap">
+          {projects &&
+            projects.map((project) => <p key={project._id}>{project.title}</p>)}
+        </div>
+      </div>
+      <div className="right col-span-1"></div>
+    </div>
+  );
 };
 
 export default Home;
