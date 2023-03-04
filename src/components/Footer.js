5ca2df143be01d44b303cb31b2a6eb0d098cc3ab
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2023-03-04 21:13:20 +0600

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="py-10 flex justify-center">
+      <p className="text-gray-400">
+        &copy;{new Date().getFullYear()} Teamtrak, All Rights Reserved
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
