b8211b1069ce659824ba8ac359d9a7d641be0807
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2023-03-04 21:06:10 +0600

    logout functionality added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
-
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useLogout } from "../hooks/useLogout";
 const Navbar = () => {
+  const { user } = useAuthContext();
+  const { logout } = useLogout();
+  const handleLogout = () => {
+    logout();
+  };
   return (
     <div className="navbar container mx-auto  h-20 flex items-center justify-between border-b border-teal-900   ">
       <Link to="/" className="logo text-2xl font-medium text-teal-400">
@@ -8,12 +14,37 @@ const Navbar = () => {
       </Link>
       <nav className="flex gap-5">
         <div className="flex gap-5">
-          <Link to={"/login"} className="hover:text-teal-400 duration-300">
-            Login
-          </Link>
-          <Link to={"/signup"} className="hover:text-teal-400 duration-300">
-            Sign Up
-          </Link>
+          <nav className="flex gap-5">
+            {!user && (
+              <div className="flex gap-5">
+                <Link
+                  to={"/login"}
+                  className="hover:text-teal-400 duration-300"
+                >
+                  Login
+                </Link>
+                <Link
+                  to={"/signup"}
+                  className="hover:text-teal-400 duration-300"
+                >
+                  Sign Up
+                </Link>
+              </div>
+            )}
+
+            {user && (
+              <div className="flex gap-5 items-center">
+                <span>{user.email}</span>
+                <button
+                  onClick={handleLogout}
+                  type="submit"
+                  className=" bg-indigo-600 text-white py-2 px-3 rounded-lg hover:bg-indigo-50 hover:text-black duration-300"
+                >
+                  Logout
+                </button>
+              </div>
+            )}
+          </nav>
         </div>
       </nav>
     </div>
